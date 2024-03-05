580b1b7e0ab33d2cddec5fa4dcbd369aa5080e9a
Jacob Barta <jacob.barta25@gmail.com>
2024-03-05 14:02:47 -0500

    add ability to search for teams manually

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,18 @@
 
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shampic/Session.dart';
 import 'package:shampic/camera.dart';
 import 'package:shampic/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'package:http/http.dart' as http;
+
+
 class Photo extends StatefulWidget {
-  const Photo({Key? key}) : super(key: key);
+  const Photo({super.key});
 
   @override
   PhotoState createState() =>
@@ -16,11 +22,16 @@ class Photo extends StatefulWidget {
 class PhotoState extends State<Photo> {
   List<String> teams = [];
 
+  bool teamHasImage = false;
+  String apiBase = "";
+  String year = "";
+
   @override
   void initState() {
     super.initState();
 
     loadTeams();
+    loadData();
   }
 
   Future<void> loadTeams() async {
@@ -32,6 +43,76 @@ class PhotoState extends State<Photo> {
       });
     }
   }
+
+  Future<void> loadData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      apiBase = prefs.getString("api")!;
+      year = prefs.getString("year")!;
+    });
+  }
+
+  void openPhotoWindow(String number, BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => Camera(number: number, camera: CameraContainer.cameras.first,)))
+        .then((value) {
+      loadTeams();
+    });
+  }
+
+  var teamNumController = TextEditingController();
+
+  void openSearchModal(BuildContext context) {
+    showDialog(context: context, builder: (BuildContext context) =>
+        AlertDialog(
+          content:  StatefulBuilder(  // You need this, notice the parameters below:
+            builder: (BuildContext context, StateSetter setState) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text("Search for Team"),
+                  TextField(
+                    decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Enter Team Number'
+                    ),
+                    controller: teamNumController,
+                    onChanged: (value) async {
+
+                      http.Response getResponse = await Session.get("$apiBase/bytes/");
+
+                      List<dynamic> existingKeysDynamic = jsonDecode(getResponse.body);
+
+                      List<String> existingKeys = existingKeysDynamic.map((e) => e as String).toList();
+
+                      bool alreadyExists = existingKeys.contains("$value-img-$year");
+
+                      //Only update the state if the input field hasn't changed since we started the pull
+                      if(value == teamNumController.text) {
+                        setState(() {
+                          teamHasImage = alreadyExists;
+                        });
+                      }
+                    },
+                    maxLength: 4,
+                    keyboardType: TextInputType.number,
+                  ),
+                  teamHasImage ?
+                  const Text("Team Has a Photo!",
+                      style: TextStyle(color: Colors.green)) :
+                  const Text("Team Doesn't Have a Photo Yet!",
+                      style: TextStyle(color: Colors.red))
+                ]);
+          }),
+            actions: <TextButton>[
+            TextButton(onPressed: () {
+                openPhotoWindow(teamNumController.text, context);
+              }, child: const Text("Take Photo of Team"),)
+            ]
+        )
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -50,10 +131,7 @@ class PhotoState extends State<Photo> {
               teams.map((e) =>
                 InkWell(
                   onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => Camera(number: e, camera: CameraContainer.cameras.first,)))
-                        .then((value) {
-                      loadTeams();
-                    });
+                    openPhotoWindow(e, context);
                   },
                   child: Container(
                     decoration: BoxDecoration(
@@ -69,7 +147,13 @@ class PhotoState extends State<Photo> {
               ,
             ),
           )]
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          openSearchModal(context);
+        },
+        child: const Icon(Icons.search),
+      ),
     );
   }
 
